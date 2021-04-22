d0a9c5d94684fc22ed375b1b5ff75c49a75e97ad
superjanosch <wehnerjanosch@gmail.com>
2021-04-22 20:58:58 +0200

    Added Raumduefte

--- include/include.js
+++ include/include.js
@@ -9,11 +9,11 @@ document.write(`
 		<ul>
 			<li><a href="/index.html">Startseite</a></li> 
 			<li><a href="/subpages/kerzen.html">Kerzen</a></li> 
-			<li><a href="/subpages/">Raumdüfte</a></li>
-			<li><a href="/subpages/">Accessoires</a></li>
-			<li><a href="/subpages/">Outlet</a></li>
-			<li><a href="/subpages/">Katalog</a></li>
-			<li><a href="/subpages/">Angebote</a></li>
+			<li><a href="/subpages/raumduefte">Raumdüfte</a></li>
+			<li><a href="/subpages/accessoires">Accessoires</a></li>
+			<li><a href="/subpages/outlet">Outlet</a></li>
+			<li><a href="/subpages/katalog">Katalog</a></li>
+			<li><a href="/subpages/angebot">Angebote</a></li>
 			<li style="float:right"><a href="/subpages/Kontakt.html">Kontakt</a></li> 
 			<li style="float:right"><a href="/subpages/">Über mich</a></li>
 		</ul>
